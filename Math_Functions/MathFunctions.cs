3eab7e887b114c14bf1726f54dcc0cce71688dd5
Samuel Lane <samuel.lane.sa@outlook.com>
2019-02-20 10:21:23 -0500

    Classwork 20 Feb 19

---
+++ Math_Functions/MathFunctions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Functions
+{
+    public class MathFunctionClass
+    {
+
+
+        //Square:: Double -> Double 
+        public double Square(double value)
+        {
+
+            return value * value;
+        }
+
+        public double Cube(double value)
+        {
+            return Math.Pow(value, 3);
+        }
+    }
+}
---
+++ Math_FunctionsTests/MathFunctionClassTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Math_Functions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shouldly;
+
+namespace Math_Functions.Tests
+{
+    [TestClass()]
+    public class MathFunctionClassTests
+    {
+        [TestMethod()]
+        public void CubeTest()
+        {
+            Assert.AreEqual(8, objectUnderTest.Cube(2));
+        }
+
+
+
+
+
+
+        //Instantiation
+        MathFunctionClass objectUnderTest = new MathFunctionClass();
+
+
+        [TestMethod()]
+        public void SquareTest()
+        {
+            Assert.AreEqual(36, objectUnderTest.Square(-6));
+            Assert.AreEqual(0, objectUnderTest.Square(0));
+            Assert.AreEqual(4, objectUnderTest.Square(2));
+            //Using ShouldBe has the same function as Assert
+            objectUnderTest.Square(6)
+                .ShouldBe(36);
+        }
+    }   
+}
