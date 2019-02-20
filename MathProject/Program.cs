05133fe822682ed7790f16a403a510c7ffe38b83
Samuel Lane <samuel.lane.sa@outlook.com>
2019-02-20 10:41:15 -0500

    Change to MathProject

--- MathProject/Program.cs
+++ MathProject/Program.cs
@@ -12,7 +12,10 @@ namespace MathProject
     {
         static void Main(string[] args)
         {
-                      
+            MathFunctionClass mathFunctions = new MathFunctionClass();
+
+            Console.WriteLine($"The value of 2pi squared is: {mathFunctions.Square(2 * Math.PI):F3}");
+            
         }
     }
 }
